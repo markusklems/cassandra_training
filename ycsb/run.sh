a6b08c8692b08536dbeaa2586b54f9e36b5a8ea4
Markus Klems <markusklems@gmail.com>
2012-12-11 22:40:11 +0100

    Update ycsb/run.sh

--- ycsb/run.sh
+++ ycsb/run.sh
@@ -5,4 +5,4 @@ workload=${2:-workloada}
 ops=${3:-500000}
 hosts=${4:-127.0.0.1}
 
-ycsb-0.1.4/bin/ycsb load cassandra-10 -P ycsb-0.1.4/workloads/$workload -s -threads 10 -target 1000 -p recordcount=$records -p operationcount=$ops -p histogram.buckets=50 -p hosts=$hosts
+ycsb-0.1.4/bin/ycsb run cassandra-10 -P ycsb-0.1.4/workloads/$workload -s -threads 10 -p recordcount=$records -p operationcount=$ops -p histogram.buckets=50 -p hosts=$hosts
