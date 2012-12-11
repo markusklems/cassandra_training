4c60dc9a79c47d9292a58f1329d55daf5292b832
markus <markusklems@gmail.com>
2012-12-11 05:16:48 -0800

    added hosts line

--- ycsb/load.sh
+++ ycsb/load.sh
@@ -2,5 +2,6 @@
 
 records=${1:-500000}
 workload=${2:-workloada}
+hosts=${3:-127.0.0.1}
 
-ycsb-0.1.4/bin/ycsb load cassandra-10 -P ycsb-0.1.4/workloads/$workload -s -threads 10 -target 2000 -p recordcount=$records -p histogram.buckets=50
+ycsb-0.1.4/bin/ycsb load cassandra-10 -P ycsb-0.1.4/workloads/$workload -s -threads 10 -target 2000 -p recordcount=$records -p histogram.buckets=50 -p hosts=$hosts
--- ycsb/run.sh
+++ ycsb/run.sh
@@ -3,5 +3,6 @@
 records=${1:-500000}
 workload=${2:-workloada}
 ops=${3:-500000}
+hosts=${4:-127.0.0.1}
 
-ycsb-0.1.4/bin/ycsb load cassandra-10 -P ycsb-0.1.4/workloads/$workload -s -threads 10 -target 1000 -p recordcount=$records ü operationcount=$ops -p histogram.buckets=50
+ycsb-0.1.4/bin/ycsb load cassandra-10 -P ycsb-0.1.4/workloads/$workload -s -threads 10 -target 1000 -p recordcount=$records ü operationcount=$ops -p histogram.buckets=50 -p hosts=$hosts
