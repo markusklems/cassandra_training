e17f9cd4df5f9d087240274945150a9452580188
Markus <dontsendmeemails>
2013-06-30 11:53:11 +0200

    Minor fix of port numbers

--- cluster/iptables/iptables_accept_output_opscenter_port.sh
+++ cluster/iptables/iptables_accept_output_opscenter_port.sh
@@ -2,9 +2,9 @@
 
 for ip in $@
 do
-  echo "Accept Opscenter traffic on port 61621 and 61622 with $ip"
+  echo "Accept Opscenter traffic on port 61620 and 61621 with $ip"
+  sudo iptables -I INPUT -p tcp -m tcp -s $ip --dport 61620 -j ACCEPT
   sudo iptables -I INPUT -p tcp -m tcp -s $ip --dport 61621 -j ACCEPT
-  sudo iptables -I INPUT -p tcp -m tcp -s $ip --dport 61622 -j ACCEPT
+  sudo iptables -I OUTPUT -p tcp -m tcp -d $ip --dport 61620 -j ACCEPT
   sudo iptables -I OUTPUT -p tcp -m tcp -d $ip --dport 61621 -j ACCEPT
-  sudo iptables -I OUTPUT -p tcp -m tcp -d $ip --dport 61622 -j ACCEPT
 done
