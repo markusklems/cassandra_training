114b8bd1425ab8a472a5ed9f4e4c5ca6bf071834
markus <markusklems@gmail.com>
2013-07-03 02:26:04 -0700

    Cassandra Java Client Examples

---
+++ cassandra-clients/src/main/java/de/cassandratraining/netflix/ModelConstants.java
@@ -0,0 +1,8 @@
+package de.cassandratraining.netflix;
+
+public class ModelConstants {
+	  public static final String COL_NAME_EMPID = "empid";
+	  public static final String COL_NAME_DEPTID = "deptid";
+	  public static final String COL_NAME_FIRST_NAME = "first_name";
+	  public static final String COL_NAME_LAST_NAME = "last_name";
+}
